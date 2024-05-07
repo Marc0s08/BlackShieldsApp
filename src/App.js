f04d02d8efcf18c983e6faf81d5c6dbd817b05d4
Marcos Eduardo <marcoseduc2019@gmail.com>
2024-05-07 17:13:41 -0300

    Adicionando alterações e arquivos não monitorados

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection } from 'firebase/firestore';
+import Page1 from './telas/Page1';
+import Page2 from './telas/Page2';
+import Page3 from './telas/Page3';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6yHbRF47f9TWUM80y3FSD1ylOFEr6JXg",
+  authDomain: "black-shields.firebaseapp.com",
+  projectId: "black-shields",
+  storageBucket: "black-shields.appspot.com",
+  messagingSenderId: "36610876059",
+  appId: "1:36610876059:web:5343e879320520c93a9c3e",
+  measurementId: "G-WJNR5S7VND"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const playersCollection = collection(db, 'players');
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <h1>Tabela de Pontos</h1>
+        <div className="buttons-container">
+          <Link to="/page1"><button>Black Shields</button></Link>
+          <Link to="/page2"><button>UD Five</button></Link>
+          <Link to="/page3"><button>Black Shields 2</button></Link>
+        </div>
+      </div>
+      <Routes>
+        <Route path="/page1" exact element={<Page1 playersCollection={playersCollection} />} />
+        <Route path="/page2" exact element={<Page2 playersCollection={collection(db, 'players2')} />} />
+        <Route path="/page3" exact element={<Page3 playersCollection={collection(db, 'players3')} />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/telas/Page3.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import { getDocs } from 'firebase/firestore';
+
+function Page3({ playersCollection }) {
+  const [playersData, setPlayersData] = useState([]);
+
+  useEffect(() => {
+    const fetchPlayersData = async () => {
+      try {
+        const querySnapshot = await getDocs(playersCollection);
+        const data = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        setPlayersData(data);
+      } catch (error) {
+        console.error("Error fetching players data: ", error);
+      }
+    };
+
+    fetchPlayersData();
+  }, [playersCollection]);
+
+  return (
+    <div className="App">
+      <h1>Tabela de Pontos - Página 3</h1>
+      <div className="tables-container">
+        <table className="left-table">
+          <thead>
+            <tr>
+              <th>Logo</th>
+              <th>Nickname</th>
+              <th>Abates</th>
+              <th>Mortes</th>
+              <th>Assistências</th>
+              <th>Conquistas</th>
+              <th>MVP</th>
+              <th>Pontuação</th>
+            </tr>
+          </thead>
+          <tbody>
+            {playersData.map((player) => (
+              <tr key={player.id}>
+                <td className="logo"><img src={player.logo} alt={player.nickname} /></td>
+                <td>{player.nickname}</td>
+                <td>{player.kills}</td>
+                <td>{player.deaths}</td>
+                <td>{player.assists}</td>
+                <td>{player.achievements}</td>
+                <td className={player.mvp ? 'MVP' : ''}>{player.mvp ? 'Sim' : 'Não'}</td>
+                <td>{calculateScore(player)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+function calculateScore(player) {
+  if (player.deaths === 0) {
+    return (player.kills + player.assists) + player.achievements + (player.mvp ? 1 : 0); // Ou qualquer outro valor que você preferir
+  } else {
+    return ((player.kills + player.assists) / player.deaths) + player.achievements + (player.mvp ? 1 : 0);
+  }
+}
+
+export default Page3;
