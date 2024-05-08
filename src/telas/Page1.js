e508e28be94e2e9cb52c4c53bb44dd87ec759730
Marcos Eduardo <marcoseduc2019@gmail.com>
2024-05-07 21:05:12 -0300

    Retiro da lista logo

--- src/telas/Page1.js
+++ src/telas/Page1.js
@@ -25,7 +25,6 @@ function Page1({ playersCollection }) {
         <table className="left-table">
           <thead>
             <tr>
-              <th>Logo</th>
               <th>Nickname</th>
               <th>Abates</th>
               <th>Mortes</th>
@@ -38,7 +37,6 @@ function Page1({ playersCollection }) {
           <tbody>
             {playersData.map((player) => (
               <tr key={player.id}>
-                <td className="logo"><img src={player.logo} alt={player.nickname} /></td>
                 <td>{player.nickname}</td>
                 <td>{player.kills}</td>
                 <td>{player.deaths}</td>
